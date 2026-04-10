a10bd33b225033ca294848b54abe7db1dfed0165
carlos-g27 <c.carlosalvarez.66@gmail.com>
2026-04-10 19:21:31 +0000

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
           <Route path='/original' element={<Original /> } />
           <Route path='/informativa' element={<Informativa /> } />
           <Route path='/usuario' element={<Usuario /> } />
-          <Route path='/equipo' element={<Equipo /> } />
+          <Route path='/equipo/:equipo' element={<Equipo /> } />
         </Routes>
         
       </Router>
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import "./style.css"
+import { Link } from 'react-router';
 
 interface Ranking {
   rank: number
@@ -8,9 +9,44 @@ interface Ranking {
   matchesPlayed: number
 }
 
+interface Estadistica {
+  position: number
+  name: string
+  value: number
+  appearances: number
+  contestantName: string
+  statName: string
+}
+
+type FiltroTipo = 'posiciones' | 'goleador' | 'asistencias' | 'amarillas' | 'atajadas'
+
+const equiposMap: Record<string, string> = {
+    "América de Cali SA": "america-de-cali",
+    "CA Bucaramanga": "atletico-bucaramanga",
+    "Club Atlético Nacional SA": "atletico-nacional",
+    "Club Deportes Tolima SA": "deportes-tolima",
+    "Asociación Deportivo Cali": "deportivo-cali",
+    "Deportivo Independiente Medellín": "independiente-medellin",
+    "Club Independiente Santa Fe": "independiente-santa-fe",
+    "CD Popular Junior FC SA": "junior",
+    "Millonarios FC": "millonarios",
+    "Once Caldas SA": "once-caldas",
+
+    "Internacional de Bogotá": "internacional-bogota",
+    "Club Llaneros SA": "llaneros",
+    "Águilas Doradas": "aguilas-doradas",
+    "Fortaleza FC": "fortaleza",
+    "Alianza FC": "alianza",
+    "Jaguares de Córdoba FC": "jaguares",
+    "Cúcuta Deportivo FC": "cucuta",
+    "Boyacá Chicó FC": "boyaca-chico",
+    "Deportivo Pereira FC": "pereira"
+  };
+
 function Home() {
   const [ranking, setRanking] = useState<Ranking[]>([])
   const [title, setTitle] = useState('')
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,7 +64,63 @@ function Home() {
     fetchData()
   }, [])
 
+
+  const [filtro, setFiltro] = useState<FiltroTipo>('posiciones')
+  const [estadisticas, setEstadisticas] = useState<Estadistica[]>([])
+  const filtros: FiltroTipo[] = ['posiciones', 'goleador', 'asistencias', 'amarillas', 'atajadas']
+
+  const [busqueda, setBusqueda] = useState('')
+
+  useEffect(() => {
+    setBusqueda('')
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://raw.githubusercontent.com/sdtibata/dataliga/refs/heads/main/${filtro}.json`)
+        const data = await res.json()
+
+        if (filtro === 'posiciones') {
+          setRanking(data.standings[0].ranking)
+          setTitle(data.standings[0].competitionName)
+        } else {
+          setEstadisticas(data)
+        }
+      } catch (error) {
+        console.error('Error cargando datos:', error)
+      }
+    }
+
+    fetchData()
+  }, [filtro])
+
+  const rankingFiltrado = ranking.filter((equipo) =>
+    busqueda.length < 3
+      ? true  // muestra todos si hay menos de 3 caracteres
+      : equipo.contestantName.toLowerCase().includes(busqueda.toLowerCase())
+  )
+
+  const estadisticasFiltradas = estadisticas.filter((jugador) =>
+    busqueda.length < 3
+      ? true  // muestra todos
+      : jugador.name.toLowerCase().includes(busqueda.toLowerCase()) ||
+      jugador.contestantName.toLowerCase().includes(busqueda.toLowerCase())
+  )
+
+
+
   return (
+    <>
+     <div className="filtros">
+        {filtros.map((onestat) => (
+          <button
+            key={onestat}
+            onClick={() => setFiltro(onestat)}
+            className={filtro === onestat ? 'activo' : ''}
+          >
+            {onestat}
+          </button>
+        ))}
+      </div>
+
     <div className="tabla-container">
       <h2>{title}</h2>
       <table className="tabla-posiciones">
@@ -44,7 +136,7 @@ function Home() {
           {ranking.map((equipo) => (
             <tr key={equipo.rank}>
               <td>{equipo.rank}</td>
-              <td>{equipo.contestantName}</td>
+              <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}> {equipo.contestantName} </Link>
               <td>{equipo.matchesPlayed}</td>
               <td>{equipo.points}</td>
             </tr>
@@ -52,6 +144,7 @@ function Home() {
         </tbody>
       </table>
     </div>
+  </>
   )
 }
 
